abdf6e85165593054b2cc70392e1ff1a98afcb15
LaievskyiSasha <zzsanchozz@gmail.com>
2020-12-01 22:48:01 +0200

    fix input email field css, change social icon size

--- src/js/script.js
+++ src/js/script.js
@@ -3,37 +3,16 @@ let notifyButton = document.getElementById('notify');
 let pingForm = document.getElementById("ping-form");
 let error = document.getElementById("email-error")
 
-emailField.addEventListener("click", emailValidation);
 
 
-//Node.insertBefore() !!!!!! для добавление элемента перед каким-то узлом а не в конец списка(для ошибки)
+
 pingForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (/.+@.+[.][a-zA-z0-9]+/g.test(emailField.value) || (emailField.value).inlcudes("\\")) {
-        error.removeAttribute("hidden");
+    if (/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(emailField.value)) {
+        error.style.visibility = "hidden";
         emailField.value = "";
+
     } else {
-        alert(`${emailField.value}`)
-        error.setAttribute("hidde", true);
+        error.style.visibility = "visible";
     }
-
-
-})
-
-function emailValidation() {
-    if (emailField.value == "Your email address...") {
-        emailField.value = "";
-    }
-}
-
-
-// sad@gmail.com
-// dqmw@as.com
-// sad@com
-// asd@.com
-// @.com
-// asd\s@gmail.com
-// sa@\gmail.com
-//[^.]+[@.+]
-//.+[^\/]@.+
-//.+@.+[.][a-zA-z0-9]+
+});
